a36b024dd8098e6f2cf9f4d0be6df26a9a1f32e7
Anquetos <t.anquetil@proton.me>
2024-05-23 14:15:59 +0200

    Removed unnecessary imports or lines of codes and added comments.

--- app/pages/root.py
+++ app/pages/root.py
@@ -1,16 +1,17 @@
-# https://www.taipy.io/posts/how-to-create-an-ai-photo-app-with-python
 from taipy.gui import Markdown
 
 
 def to_text(val):
-    return '{:,}'.format(int(val)).replace(',', ' ')
+    return "{:,}".format(int(val)).replace(",", " ")
 
-data4good_logo_path = './images/data4good-logo.svg'
-taxplorer_logo_path = './images/taxplorer-logo.svg'
-website_logo_path = './images/website-logo.svg'
-twitter_logo_path = './images/twitter-logo.svg'
-linkedin_logo_path = './images/linkedin-logo.svg'
-eutax_logo_path = './images/eutax-logo.svg'
 
-logo_path = './data/logo.png'
+# Images path
+data4good_logo_path = "./images/data4good-logo.svg"
+taxplorer_logo_path = "./images/taxplorer-logo.svg"
+website_logo_path = "./images/website-logo.svg"
+twitter_logo_path = "./images/twitter-logo.svg"
+linkedin_logo_path = "./images/linkedin-logo.svg"
+eutax_logo_path = "./images/eutax-logo.svg"
+
+# Generate page from Markdown file
 root = Markdown("pages/root.md")
