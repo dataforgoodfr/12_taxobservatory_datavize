75ea1fd87408d4beff8ce530cd9e23fd08e111a8
Anquetos <t.anquetil@proton.me>
2024-05-24 11:21:17 +0200

    Corrected logo bug.

--- app/pages/root.py
+++ app/pages/root.py
@@ -5,6 +5,11 @@ def to_text(val):
     return "{:,}".format(int(val)).replace(",", " ")
 
 
+# Path to images
+data4good_logo_path = "./images/data4good-logo.svg"
+eutax_logo_path = "./images/eutax-logo.svg"
+
+
 def goto_d4g_website(state):
     navigate(state, "https://dataforgood.fr/", tab="_blank")
 
