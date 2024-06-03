f55cbd906e9b3b124bc1ed20641d9e698a069ae5
Anquetos <t.anquetil@proton.me>
2024-06-03 11:05:57 +0200

    Added possibility to indicate the maximum year of reports to take in account.

--- app/data/data.py
+++ app/data/data.py
@@ -3,3 +3,7 @@ import pandas as pd
 # Load dataset
 path_to_data = "./data/data_final_dataviz.csv"
 data = pd.read_csv(path_to_data, sep=",", low_memory=False)
+
+# Filter dataset with the maximum year to take in account
+max_year_of_reports = 2021
+data = data.loc[data["year"] <= max_year_of_reports]
