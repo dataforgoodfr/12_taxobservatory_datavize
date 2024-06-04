efbc0407f6c0a4a9d8e943e6d92d6655b4bd0a9d
Anquetos <t.anquetil@proton.me>
2024-06-04 11:03:21 +0200

    Modified source file of data for download page.

--- app/pages/download/download.py
+++ app/pages/download/download.py
@@ -1,10 +1,10 @@
 from taipy.gui import Markdown
 
 # Initialize dataset file path
-dataset_filepath = "./data/data_final_dataviz.csv"
+dataset_filepath = "./data/EUTO_Public_CbCR_Database_2021.xlsx"
 
 # Initialise default name for file to download
-download_filename = "data_final_dataviz.csv"
+download_filename = "EUTO_Public_CbCR_Database_2021.xlsx"
 
 # Generate page from Markdown file
 download_md = Markdown("pages/download/download.md")
