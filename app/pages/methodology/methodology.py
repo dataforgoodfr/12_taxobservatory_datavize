f4b5b9c3874376541cbdd92e10ad61788da889b7
Anquetos <t.anquetil@proton.me>
2024-06-04 14:41:03 +0200

    Updated methodology page content.

--- app/pages/methodology/methodology.py
+++ app/pages/methodology/methodology.py
@@ -1,4 +1,7 @@
 from taipy.gui import Markdown
 
+# Path to equation image
+equation = "./images/transparency-score-equation.svg"
+
 # Generate page from Markdown file
 methodology_md = Markdown("pages/methodology/methodology.md")
