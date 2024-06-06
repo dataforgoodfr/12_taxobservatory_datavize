4025c3908511f544a2a2c604e3faf955c548c000
Quentin Madura <quentin.madura@gmail.com>
2024-06-06 14:55:51 +0200

    chore: optimize images

--- app/pages/country/country.py
+++ app/pages/country/country.py
@@ -8,7 +8,7 @@ from data.data import data #from dataviz.data.data import data
 
 import io
 
-header_right_image_path = 'images/pexels-ingo-joseph-1880351.png'
+header_right_image_path = 'images/pexels-ingo-joseph-1880351.jpg'
 download_icon_path = 'images/Vector.svg'
 
 selected_country = 'Mauritius'
@@ -17,7 +17,7 @@ country_md = Markdown("pages/country/country.md")
 colname_country = 'jur_name'
 selector_country = list(np.sort(data[colname_country].astype(str).unique()))
 
-df_selected_country = data[data["jur_name"]==selected_country] 
+df_selected_country = data[data["jur_name"]==selected_country]
 df_count_country = algo.number_of_tracked_reports_over_time_country(df_selected_country)
 
 colname_sector = 'sector'
@@ -41,11 +41,11 @@ def on_change_country(state):
     # state contains all the Gui variables and this is through this state variable
     # that we can update the Gui
     # state.selected_country, state.data_country_date, ...
-    # update data_country_date with the right country 
+    # update data_country_date with the right country
     # (use initialize_case_evolution)
     print("Chosen country: ", state.selected_country)
-    state.df_selected_country = data[data["jur_name"]==state.selected_country] 
-        
+    state.df_selected_country = data[data["jur_name"]==state.selected_country]
+
     # state.pie_chart = pd.DataFrame({"labels": ["Deaths", "Recovered", "Confirmed"],
     #                                 "values": [state.data_country_date.iloc[-1, 6], state.data_country_date.iloc[-1, 5], state.data_country_date.iloc[-1, 4]]})
     state.df_count_country = algo.number_of_tracked_reports_over_time_country(state.df_selected_country)
