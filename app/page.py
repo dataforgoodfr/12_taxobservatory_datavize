0e7b8bca14c21612114dacac60d28e12742b75be
gaudinnicolas <nicolasgaudin@yahoo.fr>
2024-06-13 23:21:57 +0200

    refactor: Unused Page Class and data.py

--- app/page.py
+++ app/page.py
@@ -1,13 +1,13 @@
-from taipy.gui import Markdown
-from app import config as cfg
 
+# NOT WORKING
+# Generate page from Markdown file
+# see company.py for example of use case
+# class Page:
+#     def __init__(self, id: str):
+#         self.id = id
 
-class Page:
-    def __init__(self, id: str):
-        self.id = id
-
-    def markdown(self) -> Markdown:
-        # caller = inspect.currentframe().f_back
-        # print("PAGED Called from module", caller.f_globals['__name__'])
-        # print(f"{cfg.PAGES_PATH}/{self.id}/{self.id}.md")
-        return Markdown(f"{cfg.PAGES_PATH}/{self.id}/{self.id}.md")
+#     def markdown(self) -> Markdown:
+#         # caller = inspect.currentframe().f_back
+#         # print("PAGED Called from module", caller.f_globals['__name__'])
+#         # print(f"{cfg.PAGES_PATH}/{self.id}/{self.id}.md")
+#         return Markdown(f"{cfg.PAGES}/{self.id}/{self.id}.md")
